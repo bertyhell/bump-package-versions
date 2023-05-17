30ef25081d2d628fc64e84dcf0e3dfdd95c37493
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-05-17 15:12:44 +0200

    fix(json): use tab instead of 2 spaces to ident package files

--- index.mjs
+++ index.mjs
@@ -142,11 +142,11 @@ async function bumpVersions(args) {
 
 // Write package.json files back to disk
   await Promise.all(packageJsonPathsAbsolute.map(async (path, index) => {
-    return await fs.writeFile(path, JSON.stringify(packageJsonContents[index], null, 2));
+    return await fs.writeFile(path, JSON.stringify(packageJsonContents[index], null, '\t'));
   }));
 // Write package-lock.json files back to disk
   await Promise.all(packageJsonLockPathsAbsolute.map(async (path, index) => {
-    return await fs.writeFile(path, JSON.stringify(packageJsonLockContents[index], null, 2));
+    return await fs.writeFile(path, JSON.stringify(packageJsonLockContents[index], null, '\t'));
   }));
 
   console.log(`Versions of ${packageJsonContents.length + packageJsonLockContents.length} files were updated${args.strategy === 'highest' ? ' to version ' + newVersionString : ''}.`)
